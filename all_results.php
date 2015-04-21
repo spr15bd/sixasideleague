deffcd06b2aa3f7d10411a0315359bab07d1bbe4
unknown <you@example.com>
2015-04-22 00:00:07 +0100

    Amended stylesheet, all_results page

--- all_results.php
+++ all_results.php
@@ -10,7 +10,7 @@
 <body>
 <div align="center" id="mainWrapper">
 	<?php include_once("template_header.php");?>
-	<div id="pageContent">
+	<div id="pageContentAllResults">
 		<table align=center width=700><tr valign=top height=50><td colspan=7 align=center><h1>All Results</h1></td></tr><tr><td height=30></td></tr>
 		<?php 
 		// Connect to the MySQL database
