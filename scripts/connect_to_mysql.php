3414b95059358ec5224f56f904e7fdeb74ff4f3d
Bevan Dady <bevand@ntlworld.com>
2017-08-08 19:49:12 +0100

    All

--- scripts/connect_to_mysql.php
+++ scripts/connect_to_mysql.php
@@ -3,17 +3,11 @@ $db_host = getenv('OPENSHIFT_MYSQL_DB_HOST');
 
 $db_port = getenv('OPENSHIFT_MYSQL_DB_PORT');
 
-$db_username = 'adminsdtxdFf';
+$db_username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
 
-//getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+$db_pass= getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
 
-$db_pass= 'Lq7TiuHQ8zdc';
-
-//getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-
-$db_name = 'personalprojects';
-
-//getenv('OPENSHIFT_GEAR_NAME');
+$db_name = getenv('OPENSHIFT_GEAR_NAME');
 
 $connection  = mysqli_connect("$db_host","$db_username", "$db_pass", "", $db_port) or die ("Could not connect to the mysql5.5 database.");
 mysqli_select_db($connection,"$db_name") or die ("No database");
