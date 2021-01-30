51a2edf0928eaaa84d3f792a5ec754f361218b56
spr15bd <bevandady@gmail.com>
2021-01-30 00:56:30 +0000

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html xmlns="http://www/w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name='robots' content='noindex,nofollow' />
 <title>League Home Page</title>
 <link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />
 </head>
