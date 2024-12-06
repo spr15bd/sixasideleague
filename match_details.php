9d35810b7b94fd5827c0928e2a5c6dcbc525e05f
spr15bd <bevandady@gmail.com>
2024-12-06 22:24:01 +0000

    Update match_details.php

--- match_details.php
+++ match_details.php
@@ -9,41 +9,48 @@
 <div align="center" id="mainWrapper">
 	<?php include_once("template_header.php");?>
 	<div id="pageContent">
-		<table align=center width=450><tr valign=top height=50><td align=center colspan=4><h1>Match Summary</h1></td></tr><tr><td height=30 colspan=4></td></tr>
+		<table align=center width=450>
+			<tr valign=top height=50>
+				<td align=center colspan=4><h1>Match Summary</h1></td>
+			</tr>
+			<tr>
+				<td height=30 colspan=4></td>
+			</tr>
 		<?php 
-		// Connect to the MySQL database
-		include "scripts/connect_to_mysql.php";
-		// Grab the result list ready for viewing
-
-		$sql = mysqli_query($connection, "SELECT * FROM matchevent WHERE resultid=".$_GET["matchid"]);
-		$sql2 = mysqli_query($connection, "SELECT * FROM result WHERE id=".$_GET["matchid"]);
-		$resultCount = mysqli_num_rows($sql);	//count the number of records (events occurring in the match)
-		$resultCount2 = mysqli_num_rows($sql2);
-		if ($resultCount2 > 0) {
-			while ($row = mysqli_fetch_array($sql2)) {
-				echo "<tr><td colspan=4 align=center>";
-				echo "<h3>".$row["home_team"]."  ".$row["home_goals"]." ".$row["away_team"]." ".$row["away_goals"]."</h3>";
-				echo "</td></tr>";
-				echo "<tr><td colspan=4 align=center>";
-				echo "<h3>".$row["match_date"]."</h3>";
-				echo "</td></tr>";
+			// Connect to the MySQL database
+			include "scripts/connect_to_mysql.php";
+			// Grab the result list ready for viewing
+	
+			$sql = mysqli_query($connection, "SELECT * FROM matchevent WHERE resultid=".$_GET["matchid"]);
+			$sql2 = mysqli_query($connection, "SELECT * FROM result WHERE id=".$_GET["matchid"]);
+			$resultCount = mysqli_num_rows($sql);	//count the number of records (events occurring in the match)
+			$resultCount2 = mysqli_num_rows($sql2);
+			if ($resultCount2 > 0) {
+				while ($row = mysqli_fetch_array($sql2)) {
+					echo "<tr><td colspan=4 align=center>";
+					echo "<h3>".$row["home_team"]."  ".$row["home_goals"]." ".$row["away_team"]." "
+						.$row["away_goals"]."</h3>";
+					echo "</td></tr>";
+					echo "<tr><td colspan=4 align=center>";
+					echo "<h3>".$row["match_date"]."</h3>";
+					echo "</td></tr>";
+				}
 			}
-		}
-		if ($resultCount > 0) {
-			while ($row = mysqli_fetch_array($sql)) {
-				echo "<tr>";
-				echo "<td align=center>".$row["minutes"]."</td>";
-				echo "<td align=center>".$row["event"]."</td>";
-				echo "<td align=center>".$row["player"]."</td>";
-				echo "<td align=center>".$row["team"]."</td>";
-				echo "</tr>";
+			if ($resultCount > 0) {
+				while ($row = mysqli_fetch_array($sql)) {
+					echo "<tr>";
+					echo "<td align=center>".$row["minutes"]."</td>";
+					echo "<td align=center>".$row["event"]."</td>";
+					echo "<td align=center>".$row["player"]."</td>";
+					echo "<td align=center>".$row["team"]."</td>";
+					echo "</tr>";
+				}
+			} else {
+				echo "<tr><td align=center>There were no events during this match.</td></tr>";
 			}
-		} else {
-			echo "<tr><td align=center>There were no events during this match.</td></tr>";
-		}
-		echo "<tr><td colspan=4 height=5%></td></tr>";
-		echo "<tr><td colspan=4 align=center><a href=".$_GET["frompage"].">Back</a></td></tr>";
-		echo "<tr><td height=30%></td></tr>";
+			echo "<tr><td colspan=4 height=5%></td></tr>";
+			echo "<tr><td colspan=4 align=center><a href=".$_GET["frompage"].">Back</a></td></tr>";
+			echo "<tr><td height=30%></td></tr>";
 		?>
 		</table>
 	</div>
